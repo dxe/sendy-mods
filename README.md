2bcad63aff32935166c2f17083031b13729155a1
Jake Hobbs <jakehobbs@gmail.com>
2020-07-01 08:50:53 -0700

    allow admins to export lists

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ Created by Jake Hobbs for DxE on 2020.06.25
 
 ## Features:
 - Disables "forgot password" option on login page
-- Disables "subscriber export" on subscriber list pages
-- Disables "subscriber export" on segment pages
+- Disables "subscriber export" on subscriber list pages for non-admin users
+- Disables "subscriber export" on segment pages for non-admin users
 
 ## Instructions to perform after each Sendy upgrade:
 1. Ensure folder_path is set correctly.
--- sendy-mods.py
+++ sendy-mods.py
@@ -7,13 +7,20 @@ from array import *
 # [i][0] is the file to modify, [i][1] is the string to remove
 modifications = [['login.php', '<p><a href="#forgot-form" title="" data-toggle="modal" class="recovery" id="forgot-btn"><?php echo _(\'Forgot password?\');?></a></p>'], ['subscribers.php', '<button class="btn" onclick="window.location=\'<?php echo get_app_info(\'path\');?>/includes/subscribers/export-csv.php?i=<?php echo get_app_info(\'app\');?>&l=<?php echo $lid;?>&<?php echo $filter.\'=\'.$filter_val;?>\'"><i class="icon-download-alt"></i> <?php echo _(\'Export\').\' \'.$export_title;?></button>'], ['segment.php', '<a href="<?php echo get_app_info(\'path\');?>/includes/segments/export-csv.php?i=<?php echo get_app_info(\'app\');?>&l=<?php echo $lid;?>&s=<?php echo $sid;?>" class="export-seg-csv" title="<?php echo _(\'Export CSV of this segment\');?>"><i class="icon icon-download-alt"></i> <?php echo _(\'Export\');?></a>']]
 
+adminOnlyPre = '<?php if(!get_app_info(\'is_sub_user\')):?>'
+adminOnlyPost = '<?php endif;?>'
+
 for m in modifications:
 	# read input file
 	fin = open(folder_path + m[0], "rt")
 	# read file contents to string
 	data = fin.read()
-	# replace all occurrences in the string
-	data = data.replace(m[1], '')
+	# string replacment
+	newString = ''
+	# if not login page, prepend/append it instead of removing it
+	if m[0] != 'login.php':
+		newString = adminOnlyPre + m[1] + adminOnlyPost;
+	data = data.replace(m[1], newString)
 	# close the input file
 	fin.close()
 	# open the input file in write mode
