a629009650ac6d14355a89d76307d349012ea934
Jake Hobbs <jakehobbs@gmail.com>
2021-01-30 12:20:55 -0800

    stop segments from updating automatically

--- sendy-mods.py
+++ sendy-mods.py
@@ -5,7 +5,12 @@ from array import *
 
 # modications is a 2d array where each interior array contains 2 values:
 # [i][0] is the file to modify, [i][1] is the string to remove
-modifications = [['login.php', '<p><a href="#forgot-form" title="" data-toggle="modal" class="recovery" id="forgot-btn"><?php echo _(\'Forgot password?\');?></a></p>'], ['subscribers.php', '<button class="btn" onclick="window.location=\'<?php echo get_app_info(\'path\');?>/includes/subscribers/export-csv.php?i=<?php echo get_app_info(\'app\');?>&l=<?php echo $lid;?>&<?php echo $filter.\'=\'.$filter_val;?>\'"><i class="icon-download-alt"></i> <?php echo _(\'Export\').\' \'.$export_title;?></button>'], ['segment.php', '<a href="<?php echo get_app_info(\'path\');?>/includes/segments/export-csv.php?i=<?php echo get_app_info(\'app\');?>&l=<?php echo $lid;?>&s=<?php echo $sid;?>" class="export-seg-csv" title="<?php echo _(\'Export CSV of this segment\');?>"><i class="icon icon-download-alt"></i> <?php echo _(\'Export\');?></a>']]
+modifications = [
+	['login.php', '<p><a href="#forgot-form" title="" data-toggle="modal" class="recovery" id="forgot-btn"><?php echo _(\'Forgot password?\');?></a></p>'],
+	['subscribers.php', '<button class="btn" onclick="window.location=\'<?php echo get_app_info(\'path\');?>/includes/subscribers/export-csv.php?i=<?php echo get_app_info(\'app\');?>&l=<?php echo $lid;?>&<?php echo $filter.\'=\'.$filter_val;?>\'"><i class="icon-download-alt"></i> <?php echo _(\'Export\').\' \'.$export_title;?></button>'],
+	['segment.php', '<a href="<?php echo get_app_info(\'path\');?>/includes/segments/export-csv.php?i=<?php echo get_app_info(\'app\');?>&l=<?php echo $lid;?>&s=<?php echo $sid;?>" class="export-seg-csv" title="<?php echo _(\'Export CSV of this segment\');?>"><i class="icon icon-download-alt"></i> <?php echo _(\'Export\');?></a>']],
+	['subscribe.php', 'update_segments($app_path, $list_id);']
+	['confirm.php', 'update_segments($app_path, $list_id);']
 
 adminOnlyPre = '<?php if(!get_app_info(\'is_sub_user\')):?>'
 adminOnlyPost = '<?php endif;?>'
@@ -18,7 +23,7 @@ for m in modifications:
 	# string replacment
 	newString = ''
 	# if not login page, prepend/append it instead of removing it
-	if m[0] != 'login.php':
+	if m[0] != 'login.php' | m[0] != 'subscribe.php' | m[0] != 'confirm.php' :
 		newString = adminOnlyPre + m[1] + adminOnlyPost;
 	data = data.replace(m[1], newString)
 	# close the input file
